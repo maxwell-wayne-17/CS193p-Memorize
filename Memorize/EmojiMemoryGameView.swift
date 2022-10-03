fa5d25d72b14d18bb3724bf8690204a5bf34ee36
Maxwell Wayne <mwayne@alarm.com>
2022-10-03 15:15:41 -0400

    Finished Lecture 7, added implicit animation to make emojis spin

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -33,22 +33,23 @@ struct CardView: View {
     let card: EmojiMemoryGame.Card
     
     var body: some View {
-        GeometryReader(content: { geometry in
+        GeometryReader{ geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90) )
-                        .padding(DrawingConstants.circlePadding).opacity(DrawingConstants.circleOpacity)
-                    Text(card.content).font( font(in: geometry.size) )
-                } else if card.isMatched {
-                    shape.opacity(0)
-                }
-                else {
-                    shape.fill()
-                }
+                Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90) )
+                    .padding(DrawingConstants.circlePadding)
+                    .opacity(DrawingConstants.circleOpacity)
+                Text(card.content)
+                    .rotationEffect( Angle.degrees( card.isMatched ? 360 : 0 ) )
+                    .animation( Animation.linear( duration: 1).repeatForever(autoreverses: false), value: card.isMatched )
+                    .font( Font.system(size: DrawingConstants.fontSize) )
+                    .scaleEffect( scale( thatFits: geometry.size ) )
             }
-        })
+            .cardify(isFaceUp: card.isFaceUp)
+        }
+    }
+    
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
     
     private func font(in size: CGSize) -> Font {
@@ -56,9 +57,8 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.7
+        static let fontSize: CGFloat = 32
         static let circlePadding: CGFloat = 5
         static let circleOpacity = 0.5
     }
